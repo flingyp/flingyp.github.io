fcbae51110669bdc04421217af0f023079b51fc5
YePeng <1213563369@qq.com>
2024-01-26 17:53:24 +0800

    chore: init

--- .vitepress/theme/index.ts
+++
@@ -1,7 +0,0 @@
-import DefaultTheme from "vitepress/theme";
-// 站点主页，Logo图片样式修改
-import "./styles/HomeHeroImage.css";
-// 代码块相关样式
-import "./styles/CodeStyle.css";
-
-export default DefaultTheme;
--- README.md
+++
@@ -1,1 +0,0 @@
-<h1 align="center">FlingYP Personal Blog</h1>
