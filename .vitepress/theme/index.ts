74faee33efa67f32dd8d993b5a8761c72db35b27
YePeng <1213563369@qq.com>
2024-01-29 14:47:42 +0800

    chore: 添加暗黑自定义过渡动画

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,16 +2,16 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import "./styles/style.css";
+import Layout from "./layout/Layout.vue";
+import "./styles/style.scss";
 import "./styles/font.scss";
 import "./styles/overwrite.scss";
 
 export default {
   extends: DefaultTheme,
+  // https://vitepress.dev/guide/extending-default-theme#layout-slots
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    });
+    return h(Layout, null, {});
   },
   enhanceApp({ app, router, siteData }) {
     // ...
--- .vitepress/theme/views/Home.vue
+++ .vitepress/theme/views/Home.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>
+  <div>
     <div class="PuHui_Thin">这是一个基于 Vitepress 搭建的博客站点</div>
     <div class="PuHui_Light">这是一个基于 Vitepress 搭建的博客站点</div>
     <div class="PuHui_Regular">这是一个基于 Vitepress 搭建的博客站点</div>
@@ -12,7 +12,7 @@
     <div class="PuHui_ExtraBold">这是一个基于 Vitepress 搭建的博客站点</div>
     <div class="PuHui_Heavy">这是一个基于 Vitepress 搭建的博客站点</div>
     <div class="PuHui_Black">这是一个基于 Vitepress 搭建的博客站点</div>
-  </h1>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
