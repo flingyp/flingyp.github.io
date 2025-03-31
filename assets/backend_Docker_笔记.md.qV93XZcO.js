d104734e2741d3ee7ded8c59de65882e634c098b
flingyp <flingyp@163.com>
2025-03-31 02:18:45 +0000

    Deploying to gh-pages from @ flingyp/flingyp.github.io@03d0bdacbe2c23235851a229b47bde09260a37e4 🚀

---
+++ assets/backend_Docker_笔记.md.qV93XZcO.js
@@ -0,0 +1,55 @@
+import{_ as a,c as i,o as n,ag as p}from"./chunks/framework.BhcQuJV0.js";const r=JSON.parse('{"title":"笔记","description":"","frontmatter":{},"headers":[],"relativePath":"backend/Docker/笔记.md","filePath":"backend/Docker/笔记.md","lastUpdated":1743387406000}'),l={name:"backend/Docker/笔记.md"};function e(t,s,k,h,d,c){return n(),i("div",null,s[0]||(s[0]=[p(`<h1 id="笔记" tabindex="-1">笔记 <a class="header-anchor" href="#笔记" aria-label="Permalink to &quot;笔记&quot;">​</a></h1><h2 id="部署一个-vue3-项目" tabindex="-1">部署一个 Vue3 项目 <a class="header-anchor" href="#部署一个-vue3-项目" aria-label="Permalink to &quot;部署一个 Vue3 项目&quot;">​</a></h2><p>在你的项目根目录下创建一个 <code>dockerfile</code> 文件，用来定义 Docker 镜像的构建规则。</p><div class="language-dockerfile vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">dockerfile</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 使用 Node.js 16 作为基础镜像</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">FROM</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> node:16</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 将当前工作目录设置为/app</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">WORKDIR</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> /app</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 将 package.json 和 package-lock.json 复制到 /app 目录下</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">COPY</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> package*.json ./</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 运行 npm install 安装依赖</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">RUN</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> npm install --registry=https://registry.npmmirror.com</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 将源代码复制到 /app 目录下</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">COPY</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> . .</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 打包构建</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">RUN</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> npm run build</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 将构建后的代码复制到 nginx 镜像中</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">FROM</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> nginx:latest</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">COPY</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> --from=0 /app/dist /usr/share/nginx/html</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 暴露容器的 9999 端口，此处其实只是一个声明作用，不写的话也可以，后面运行容器的</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># docker run --name container_name -p &lt;host_port&gt;:&lt;container_port&gt;命令中container_port可以覆盖此处的声明，不写就默认80端口</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">EXPOSE</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 9999</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 启动 nginx 服务</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">CMD</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> [</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;nginx&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">, </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;-g&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">, </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;daemon off;&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">]</span></span></code></pre></div><p>随后使用 <code>docker build -t 镜像名称 .</code> 来构建镜像。<code>.</code> 表示当前目录，即 Dockerfile 所在的目录。</p><p>紧接着在创建一个 <code>.dockerignore</code> 文件，用来忽略一些不需要打包进镜像的文件，比如 <code>.git</code> 目录、<code>node_modules</code> 目录等。</p><div class="language-dockerfile vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">dockerfile</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">node_modules</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">.DS_Store</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">npm-debug.log*</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">yarn-debug.log*</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">yarn-error.log*</span></span></code></pre></div><p>最后使用 <code>docker run -d -p 9999:80 镜像名称</code> 来运行容器，其中 <code>-p 9999:80</code> 表示将宿主机的 9999 端口映射到容器的 80 端口，<code>镜像名称</code> 是你前面构建的镜像的名称。</p><h2 id="部署一个-nuxt3-项目" tabindex="-1">部署一个 Nuxt3 项目 <a class="header-anchor" href="#部署一个-nuxt3-项目" aria-label="Permalink to &quot;部署一个 Nuxt3 项目&quot;">​</a></h2><p>在你的项目根目录下创建一个 <code>dockerfile</code> 文件，用来定义 Docker 镜像的构建规则。</p><div class="language-dockerfile vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">dockerfile</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 使用 Node.js 作为基础镜像</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">FROM</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> node:20</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 将当前工作目录设置为/app</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">WORKDIR</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> /app</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 将 package.json 和 pnpm-lock.yaml 复制到 /app 目录下</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">COPY</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> package.json pnpm-lock.yaml ./</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 安装依赖并且安装pnpm</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">RUN</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> npm install -g pnpm &amp;&amp; pnpm install</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 将源代码复制到 /app 目录下</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">COPY</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> . .</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 打包构建</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">RUN</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> pnpm run build</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 启动服务</span></span>
+<span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">CMD</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> [</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;node&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">, </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;.output/server/index.mjs&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">]</span></span></code></pre></div><p>随后使用 <code>docker build -t 镜像名称 .</code> 来构建镜像。<code>.</code> 表示当前目录，即 Dockerfile 所在的目录。</p><p>紧接着在创建一个 <code>.dockerignore</code> 文件，用来忽略一些不需要打包进镜像的文件，比如 <code>.git</code> 目录、<code>node_modules</code> 目录等。</p><div class="language-dockerfile vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">dockerfile</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">/.nuxt</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">/.output</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">/node_modules</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">.gitignore</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">README.md</span></span></code></pre></div><p>最后使用 <code>docker run -d -p 9999:3000 镜像名称</code> 来运行容器，其中 <code>-p 9999:3000</code> 表示将宿主机的 9999 端口映射到容器的 3000 端口，<code>镜像名称</code> 是你前面构建的镜像的名称。</p>`,15)]))}const E=a(l,[["render",e]]);export{r as __pageData,E as default};
